dc16135f9818961897cf6db078da65842451be09
SergeyPegov <pegov.serezha@yandex.ru>
2023-01-25 18:21:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, Body, UploadFile, Response
+from fastapi.responses import FileResponse
+import pandas as pd
+import csv
+import os
+
 
 app = FastAPI()
 
-
 @app.post("/case_sensitive")
-async def dublic(q: list):
+def dublic(q: list):
     x = 0
     i = 0
     dup = [y for i, y in enumerate(q) if i != q.index(y)]
@@ -20,4 +25,52 @@ async def dublic(q: list):
     return res
 
 @app.post("/upload/{file.name}")
-async def 
+def s_files(file_name,response: Response, files: List[UploadFile]):
+    uns_files = []
+    for file in files:
+        if file.filename.partition('.')[-1] not in ["csv", "json"]:
+            uns_files.append(file.filename)
+
+    if bool(uns_files):
+        response.status_code = 415
+        return uns_files
+
+    if not os.path.isdir('data'):
+        os.mkdir('data')
+
+    dat = []
+    for file in files:
+        dt_r = pd.read_csv(file.file, sep=';') if file.content_type == "text/csv" else pd.read_json(file.file)
+        dat.append(dt_r)
+        dt_r = pd.concat(dat, dt_r)
+    dt_r = pd.concat(dat)
+    writer_file(file_name,dt_r)
+
+
+@app.post("/load/{file_name}")
+def new_file(file_name, response: Response):
+    if search(file_name):
+        return FileResponse(search(file_name))
+    else:
+        response.status_code = 404
+        return file_name + ".cvs"
+
+data_dir = 'data/' if os.environ.get('DATA_DIR') is None else os.environ.get('DATA_DIR')
+
+def search(file_name):
+    if os.path.isfile(data_dir + file_name + '.csv'):
+        return data_dir + file_name + '.csv'
+    else:
+        return False
+
+def writer_file(filename, data):
+    path = search(filename)
+    if path:
+        file_w = pd.read_csv(path, sep=';')
+        data = pd.concat([file_w, data])
+    else:
+        path = data_dir + filename + '.csv'
+        f = open(path, 'w+')
+        f.close()
+
+        data.to_csv(path, sep=';')
